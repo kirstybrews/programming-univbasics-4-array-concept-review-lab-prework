9dc322e0108f7c27ea967f3147446b71d53fa3c0
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-10-12 19:09:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,5 +23,13 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  
+  counter = 0 
+  min_value = 100
+  while array[counter] do
+    if array[counter] < min_value
+      min_value = array[counter]
+    end
+    counter += 1
+  end
+  return min_value
 end
